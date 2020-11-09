60bcc02ab37a0f0ea1df7b0f9ef20cf42ab6eec5
akmubi <akmubi@yandex.ru>
2020-11-09 18:34:59 +0500

    Вывод однородного Пуассоновского процесса

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
+#include <malloc.h>
 #include <stdio.h>
+#include "poisson_process.h"
+#include "unrolled_list.h"
 
 int main(void)
 {
-	puts("Hello, World!");
+	double T = 100.0;
+	double lambda = 1.0;
+	int k = 5;
+	int I = 0;
+	struct UnrolledList list;
+	list_init(&list);
+	int intervals = (int)(T / k);
+	unsigned int *N = malloc(intervals * sizeof(unsigned int));
+	homogeneous_generate(T, lambda, k, &I, &list, N);
+
+	puts("List:");
+	list_print(&list);
+
+	puts("N:");
+	for (int i = 0; i < intervals; ++i)
+		printf(" %u", N[i]);
+	putchar('\n');
+	free(N);
+	
+	printf("I = %d\n", I);
+	list_terminate(&list);
 	return 0;
 }
