3078aa75ce030ad83ad630f50fd3abab93de3803
akmubi <akmubi@yandex.ru>
2020-11-11 12:45:31 +0500

    Тесты генерации процессов

--- main.c
+++ main.c
@@ -1,30 +1,71 @@
 #include <malloc.h>
 #include <stdio.h>
+#include <math.h>
+
+#include "MLKG/statistical_tests.h"
 #include "poisson_process.h"
 #include "unrolled_list.h"
 
+// #define HOMOGENEOUS
+
+#if !defined(HOMOGENEOUS)
+static double lambda_func(double t, double lambda)
+{
+	return lambda * fabs(sin(t));
+}
+#endif
+
+
 int main(void)
 {
-	double T = 100.0;
+	double T = 20.0;
 	double lambda = 1.0;
-	int k = 5;
-	int I = 0;
-	struct UnrolledList list;
-	list_init(&list);
-	int intervals = (int)(T / k);
-	unsigned int *N = malloc(intervals * sizeof(unsigned int));
-	homogeneous_generate(T, lambda, k, &I, &list, N);
+	int k = 1;
+	printf("T = %7.4lf\n", T);
+	printf("lambda = %7.4lf\n", lambda);
+	printf("k = %d\n", k);
 
-	puts("List:");
-	list_print(&list);
+	int I = 0;
+	struct UnrolledList list, N;
+	list_init(&list);
+	list_init(&N);
+
+#if defined(HOMOGENEOUS)
+	homogeneous_generate(T, lambda, k, &I, &list, &N);
+#else
+	inhomogeneous_generate(T, lambda, &lambda_func, k, &I, &list, &N);
+	puts("lambda_func = lambda * |sin(t)|");
+#endif
+
+	puts("S:");
+	int i = 0;
+	struct Node *iterator = list.root;
+	do
+	{
+		for (int j = 0; j < iterator->count; ++j)
+			printf("%3d : %6.4lf\n", i * ARRAY_SIZE + j + 1, iterator->array[j]);
+		iterator = iterator->next;
+		++i;
+	}
+	while (iterator != list.root);
 
 	puts("N:");
-	for (int i = 0; i < intervals; ++i)
-		printf(" %u", N[i]);
-	putchar('\n');
-	free(N);
+	int freq_count = list_get_size(&N);
+	unsigned int *freq = malloc(freq_count * sizeof(unsigned int));
+	for (int i = 0; i < freq_count; ++i)
+	{
+		freq[i] = (unsigned int)list_get_by(&N, i);
+		printf("%u\n", freq[i]);
+	}
 	
 	printf("I = %d\n", I);
 	list_terminate(&list);
+
+	double chi_square = 0.0;
+	int deg_freedom = 0;
+
+	pearson_poisson(freq, freq_count, I, &chi_square, &deg_freedom);
+	free(freq);
+	list_terminate(&N);
 	return 0;
 }
